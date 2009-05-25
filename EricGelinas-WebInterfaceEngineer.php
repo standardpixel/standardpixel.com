03e8f55e2da416ce1db60c37f1ef00272f6120a0
Eric Gelinas <eric@li45-79.members.linode.com>
2009-05-25 19:46:12 +0000

    added resume to new site

---
+++ EricGelinas-WebInterfaceEngineer.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	<link rel="shortcut icon" href="/img/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" href="/img/apple-touch-icon.png"/>
+	<link href="http://feeds.feedburner.com/StandardPixel" rel="alternate" type="application/rss+xml" title="StandardPixel RSS Feed" />
+	<title>StandardPixel.com - Web Development Services</title>
+	<link rel="stylesheet" type="text/css" href="http://dev.s.standardpixel.com/d/style.php">
+</head>
+
+<body>	<div id="container" class="stapix millbahn elastic">
+		<div id="header">
+			<h1>
+				<span class="standard">standard</span><span class="pixel">pixel</span><span class="com">.com</span>
+			</h1>
+			<h2>Web Application Development Blog</h2>
+		</div>
+		<div id="content_1" class="area col0">
+					
+			<div class="article module col0">
+			
+				<h4>Summary</h4>
+				<p>To pursue a position that will apply and enhance my web standards experience. Focus my understanding of user interaction to create engaging and innovative web interfaces.</p>
+				
+				<h4>Experience</h4>
+				<p><b>2007 to Present Front-End Engineer - Yahoo!, Inc Burbank, Ca</b></p>
+				<p>
+					<ul>
+						<li>Created JavaScript based interface components distributed through a packaged framework</li>
+						<li>Enforce Web Standards from design to development</li>
+						<li>Created scaleable CSS and Markup for tempting</li>
+						<li>Helped deploy a Scrum strategy within a project, including facilitation of communication across skill-set and organization</li>
+						<li>Mentored colleagues on Object Oriented JavaScript and Scrum</li>
+						<li>Extended the YUI framework within a project specific library and worked directly with the YUI core team to resolve issues.</li>
+					</ul>
+					<ul>
+						<li><a href="http://apt.yahoo.com/">http://apt.yahoo.com/</a></li>
+					</ul>
+				</p>
+				
+				<p><b>2004 to 2007 Markup Engineer - EarthLink, Inc Pasadena, Ca</b></p>
+				<p>
+					<ul>
+						<li>Led interface development of a Map-based search product. Leveraged YUI, Prototype JavaScript Framework, and Scriptaculous frameworks to enhance the search an mapping functionality of the Microsoft Virtual Earth API.</li>
+						<li>Applied knowledge of Agile development concepts to create a Web 2.0 portal that will replace the current EarthLink web portal.</li>
+						<li>Key developer of a social bookmarking site. This application was built using the Ruby on Rails framework. RJS and JSON were used to facilitate an AJAX enabled application of Model-view-controller.</li>
+						<li>Ongoing HTML, Javascript, and CSS enhancements to a web portal with a user base of over 4 million subscribers. Optimized images delivered in photoshop format from UI design group. Developed Apache Velocity templates that enable a content management tool.</li>
+					</ul>
+					<ul>
+						<li><a href="http://start.earthlink.net/">http://start.earthlink.net/</a></li>
+						<li><a href="http://earthlink.net/">http://earthlink.net/</a></li>
+					</ul>
+				</p>
+				
+				<p><b>2001 to 2004 Production Coordinator - EarthLink, Inc Pasadena, Ca</b></p>
+				<p>
+					<ul>
+						<li>Ongoing HTML, Javascript, and CSS enhancements to the primary marketing site of a national ISP.</li>
+						<li>Primary web designer for a web site featuring EarthLink's Eco-Challenge team.</li>
+						<li>Led an initiative to create a workflow system to manage content update requests for EarthLink's portal products and improve staff efficiency. Acted as primary developer and server support for a PHP and MySQL-based application.</li>
+					</ul>
+				</p>
+				
+				<p><b>1998 to 2001 Technical Support - EarthLink, Inc Pasadena, Ca</b></p>
+				<p>
+					<ul>
+						<li>Supervisor of 11 technical support representatives. Ensured customer satisfaction through direct customer communication and employee development.</li>
+						<li>Primary interface developer of a web application suite developed to facilitate communication between 4000 call center employees and corporate communications. This application was developed using ASP and Microsoft Exchange.</li>
+					</ul>
+				</p>
+				
+				<p><b>1997 to 1998 Web Designer - Career Explorers, Inc Chatsworth, Ca</b></p>
+				<p>
+					<ul>
+						<li>Designed and developed primary website of a direct mail catalog.</li>
+					</ul>
+				</p>
+				
+				<h4>Training</h4>
+				
+				<p><b>2005 Agile Web Development with Rails</b></p>
+				<p>
+					<ul>
+						<li>Dave Thomas and Mike Clark of Pragmatic Studio</li>
+					</ul>
+				</p>
+				
+				<p><b>2007 Scrum Team Training</b></p>
+				<p>
+					<ul>
+						<li>Trail Ridge Consulting</li>
+					</ul>
+				</p>
+				
+			</div>
+		
+		</div>	
+		<div id="navigation" class="module col2 sitenav">
+			<h3>Navigation</h3>
+			<ul>
+				<li><a href="http://standardpixel.com/EricGelinas-WebInterfaceEngineer.php">Home</a></li>
+				<li><a href="http://blog.standardpixel.com">Blog</a></li>
+				<li><a href="http://github.com/standardpixel/standarpixel.com/">Portfolio</a></li>
+			</ul>
+		</div>
+		<div id="footer" class="module">
+			<div>&copy;&nbsp;2009&nbsp;StandardPixel LLC, All Rights Reserved.</div>
+			<div>&nbsp;-&nbsp;</div>
+			<div>
+				<a href="mailto:info@standardpixel.com">info@standardpixel.com</a>
+				&nbsp;[ <a href="http://github.com/standardpixel/standarpixel.com/">Source Code</a> ]
+			</div>
+		</div>
+	</div>
+	<script src="http://dev.s.standardpixel.com/d/behavior.php"></script>
+	
+	</body>
+</html>
