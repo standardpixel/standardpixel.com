bfc5185b03874f3b9128d55717abb71e4b45149e
Eric Gelinas <eric@standardpixel.com>
2010-11-16 08:14:05 +0000

    new layout boilerplate

--- index.php
+++ index.php
@@ -3,28 +3,82 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-		<title></title>
+		<title>StandardPixel.com - Things which interest Eric Gelinas</title>
 		<meta name="description" content="">
 		<meta name="author" content="">
 
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+		
+		<script type="text/javascript" src="http://use.typekit.com/tkt0mro.js"></script>
+		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		
+		<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/2.8.2r1/build/reset/reset-min.css">
+		
+		<style>
+			html, body {
+				background-color:#343B4F;
+				font-family:"raleway-1", "raleway-2", helvetica, arial sans-serif;
+				font-size:12px;
+			}
+			
+			.narcissistic h1, .narcissistic h2 {
+				color:#6CDFEA;
+				font-size:44px;
+				font-weight:normal;
+				text-shadow: 3px 3px 1.5em #000;
+			}
+			
+			.narcissistic #container {
+				width:1024px;
+				margin:0 auto;
+			}
+			
+			.narcissistic header {
+				color:#6CDFEA;
+				margin-top:20px;
+			}
+			
+			.narcissistic footer {
+				color:#6CDFEA;
+				margin-top:20px;
+				text-align:right;
+			}
+			
+			.narcissistic #main {
+				background-color:#FFF;
+				margin-top:20px;
+				padding:10px;
+				-moz-border-radius: 3px; -webkit-border-radius: 3px; border-radius: 3px;
+				-moz-box-shadow: 3px 3px 1.5em black; -webkit-box-shadow: 3px 3px 1.5em black; box-shadow: 3px 3px 1.5em black;
+			}
+			
+			.narcissistic #main #main-content, .narcissistic #main #main-image {
+				display:inline-block;
+			}
+			
+			
+		</style>
+		
 	</head>
 
-	<body>
+	<body class="narcissistic">
 
 		<div id="container">
 			<header>
-				<span>Hello, I am Eric Gelinas and recently I...</span>
+				<h1>Hello, I am Eric Gelinas and recently I...</h1>
 			</header>
 
 			<div id="main">
-				<div>Image here</div>
-				<div>Content here</div>
+				<div id="main-image">
+					<img src="http://farm5.static.flickr.com/4092/5124639215_4d2f5ff999.jpg">
+				</div>
+				<div id="main-content">
+					<h2>Favorited a photo on Flickr</h2>
+				</div>
 			</div>
 
 			<footer>
-				<div>StandardPixel.com</div>
+				<h2>StandardPixel.com</h2>
 				<div>Things which interest me</div>
 				<div>[Twitter][AudioScrobbler][Reader][RSS][Delicious][Flickr]</div>
 			</footer>
