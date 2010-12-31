680477a96eec12964971079bf3f8e7770ed9fd45
Eric Gelinas <eric@standardpixel.com>
2010-12-31 18:38:31 +0000

    a little directory pruning

--- include/funk.php
+++ include/funk.php
@@ -1,19 +1,3 @@
 <?php 
-	class SP {
-	    public $root_path = "/var/www/domains/ComStandardpixelDev/";
-	    public $media_path = "/";
-
-	    function include_header($title){
-		  	$sp_title = $title;
-		  	include("header.html");
-		 	}
-		
-			function include_nav(){
-		  	include("nav.html");
-		 	}
-		
-			function include_footer(){
-		  	include("footer.html");
-		 	}
-	}
+	//This just has to be here until the new site launches
  ?>
