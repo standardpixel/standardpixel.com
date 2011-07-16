a046a474ac38c6fb2f873a74e5a5e002c936ccd9
Eric Gelinas <eric@standardpixel.com>
2011-07-16 05:39:17 +0000

    html5 markup for domestica version

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-		<title>StandardPixel.com - The work and curations of Eric Gelinas</title>
+		<title>StandardPixel.com - Curation of _things_ by Eric Gelinas</title>
 		<meta name="description" content="">
 		<meta name="author" content="">
 
 		<meta name="viewport" content="width=device-width, initial-scale=0.9">
 		       
-		<script type="text/javascript" src="http://use.typekit.com/tkt0mro.js"></script>
-		<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+		<!--script type="text/javascript" src="http://use.typekit.com/tkt0mro.js"></script!-->
+		<!--script type="text/javascript">try{Typekit.load();}catch(e){}</script-->
 		                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
 		<style>
 			html, body {
@@ -18,173 +18,51 @@
 				font-size:12px;
 				font-weight:normal;
 			}
-			
-			.extraordinary a, .extraordinary a:visited {text-decoration:none;}
-			
-			.extraordinary h1, .extraordinary h2 {
-				font-size:44px;
-				font-weight:300;
-				margin:0;
-				padding:0;
-				font-family:"museo-sans-1", "museo-sans-2", helvetica, arial sans-serif;
-			}
-			
-			.extraordinary p {
-				margin:0;
-			}
-			
-			.extraordinary .logo{
-				font-size:50px;
-				font-family:helvetica, arial sans-serif;
-				font-weight:500;
-			} 
-			.extraordinary .logo .standard{color:#b00;}
-			.extraordinary .logo .pixel{color:#000;}
-			.extraordinary .logo .com{color:#aaa;}
-			
-			.extraordinary #container {
-				width:1023px;
-				margin:0 auto;
-			}
-			
-			.extraordinary header {
-				margin-top:20px;
-				//text-shadow: black 0 0 .5em;
-			}
-			
-			.extraordinary footer {
-				margin-top:20px;
-				text-align:right;
-				//text-shadow: black 0 0 .5em;
-			}
-			
-			.extraordinary footer p {
-				margin:0px;
-				margin-top:11px;
-				font-family:"museo-sans-1", "museo-sans-2", helvetica, arial sans-serif;
-				font-size:12px;
-				font-weight:normal;
-			}
-			
-			.extraordinary footer div.socialshit {
-				margin-top:15px;
-			}
-			
-			.extraordinary #main {
-				margin-top:20px;
-				padding:6px 11px;
-				-moz-border-radius: 3px; -webkit-border-radius: 3px; border-radius: 3px;
-				position:relative;
-				height:388px;
-			}
-			
-			.extraordinary #main h3 {
-				font-size:24px;
-				font-weight:300;
-			}
-			
-			.extraordinary #main #main-image {
-				position:absolute;
-				top:11px;
-				left:11px;
-				width:495px;
-				height:378px;
-				overflow:hidden;
-				
-			}
-			
-			.extraordinary #main #main-image img {
-				margin:auto;
-			}
-			
-			.extraordinary #main #main-content {
-				margin-left:506px;
-			}
-			
-			.extraordinary #main .photo-title {
-				text-weight:bolder;
-			}
-			
-			.extraordinary #main .photo-description {
-				margin-top:2px;
-				line-height:17px;
-			}
-			
-			.extraordinary #main .photo-credit {
-				font-weight:bolder;
-				margin-top:5px;
-			}
-			
-			.extraordinary #main .service-logo {
-				position:absolute; bottom:9px;
-			}
-			
-			.extraordinary #main .service-credit {
-				position:absolute;bottom:17px;left:546px;
-			}
-			
-			.extraordinary #main .advance-button {
-				display:none;
-			}
-			
-			.extraordinary #main:hover .advance-button {
-				display:block;
-				position:absolute;bottom:6px;right:11px;
-				font-weight:bolder;font-size:20px;font-family:helvetica, arial sans-serif;
-			}
-			
-			.extraordinary #main:hover .advance-button:hover {
-				cursor: pointer;
-				cursor: hand;
-			}
-			
-			/* colours */
-			html, body.winter {background-color:#F2F4F2;}
-			.winter h1, .winter h2 {color:#0E0E2F}
-			.winter a:hover {text-shadow: #3300fb 0 0 1px;}
-			.winter header {color:#0E0E2F}
-			.winter footer {color:#0E0E2F}
-			.winter #main {-moz-box-shadow: black 0 0 3em; -webkit-box-shadow: black 0 0 3em; box-shadow: black 0 0 3em;}
-			.winter #main {background-color:#FFF;}
-			.winter #main:hover .advance-button {text-shadow: #0E0E2F 0 0 3px;color:#eee;}
-			.winter #main:hover .advance-button:hover {text-shadow: blue 0 0 3px;}
-			
 		</style>
 		
 	</head>
 
-	<body class="extraordinary winter">
+	<body class="domestica summer">
 
 		<div id="container">
 			<header>
-				<h1>One extraordinary thing:</h1>
+				<div id='logo'>
+					<div class="illustration">
+						<div class="block-1"></div>
+						<div class="block-2"></div>
+						<div class="block-3"></div>
+						<div class="block-4"></div>
+					</div>
+					<h2>Standard Pixel</h2>
+				</div>
+				<h1>Curation of <i>things</i> by Eric Gelinas</h1>
 			</header>
 
-			<div id="main">
-				<div id="main-image">
-					<img src="http://farm5.static.flickr.com/4092/5124639215_4d2f5ff999.jpg">
-				</div>
-				<div id="main-content" class="service-flickr action-favorite">
-					<p class="photo-title" style="font-size:24px;"><a href="#">Bla Bla Bla Bla</a></p>
-					<p class="photo-description">
-						Lorizzle ipsizzle dolor sit black, yo mamma adipiscing elit. Mofo sapizzle velizzle, aliquet volutpizzle, suscipit mofo, mammasay mammasa mamma oo sa vizzle, arcu. Pellentesque brizzle dang. Sizzle erizzle. Pimpin' at funky fresh its fo rizzle turpis tempizzle the bizzle. Maurizzle pellentesque get down get down sheezy turpizzle. Vestibulum izzle tortor. Pellentesque brizzle rhoncus crackalackin. In hac dawg platea dictumst. I saw beyonces tizzles and my pizzle went crizzle dapibizzle. Black tellus urna, izzle dope, doggy rizzle, eleifend da bomb, nunc. Get down get down suscipizzle. Integizzle semper sure sed yo.
-					</p>
-					<p class="photo-credit">by <a href="#">Some Jackass</a></p>
+			<section>
+				<article>
+					<figure>
+						<img src="http://farm6.static.flickr.com/5067/5624262088_2ecf2e1f21_z.jpg" width="640px" height="426px">
+					</figure>
+					<h2>Here is one of my <a href="http://www.flickr.com/photos/standardpixel/favorites/">favorite</a> photos</h2>
+					<figcaption>
+						<h3>Union Station</h3>
+						<p>Historic Los Angeles Union Station was purchased by the Los Angeles County MTA from from Catellus/Prologis on 4/14/2011 for a reported $75 million. Opened in May 1939, Union Station is known as the "Last of the Great Railway Station" built in the United States.</p>
+
+						<p>Union Station was partially designed by the father and son team of John Parkinson and Donald B. Parkinson, or the Parkinsons, assisted by a group of supporting architects, including the famous Jan van der Linden. The Parkinsons also designed Los Angeles City Hall.</p>
+
+						<p>The MTA headquarters building can also be seen behind Union Station to the right.</p>
+
+						<p>See also: <a href="http://thesource.metro.net/2011/04/14/metro-tonight-officially-becomes-new-owner-of-los-angeles-union-station/">[a page not on standardpixel.com]</a></p>
+
+						
+					</figcaption>
 					
-					<div class="service-logo"><a href="http://flickr.com/standardpixel"><img src="/img/flickr.png" alt="Flickr"></a></div>
-					<p class="service-credit"><a href="http://flickr.com/standardpixel/favorites">I Favorited this photo on flickr</a></p>
-					<div class="advance-button" title="Show something else...">↻</div>
-				</div>
-			</div>
+				</article>
+			</section>
 
 			<footer>
-				<div class="logo"><span class="standard">standard</span><span class="pixel">pixel</span><span class="com">.com</span></div>
-				<p>Designed and developed by Eric Gelinas</p>
-				<p>
-					<a href="#">Resumé</a>&nbsp;•&nbsp;
-					<a href="#">Blog</a>&nbsp;•&nbsp;
-					<a href="#">Source</a>
-				</p>
+				<nav><span>Blog</span><span>A little about Eric Gelinas</span><span>Source</span></nav>
+				<p>Thanks for stopping by.</p>
 			</footer>
 		</div>
 
