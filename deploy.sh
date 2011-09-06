d5e6f6974fcf35592c310644d0dc3fd9c3aeff99
Eric Gelinas <eric@standardpixel.com>
2011-09-06 06:41:21 +0000

    stupid simple deploy script

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+echo "Deploying to standardpixel.com..."
+echo "+++++++++++++++++++++++++++++++++"
+cp -v style_domestica_summer.css /var/www/
+cp -v index.php /var/www/
+cp -v EricGelinas-WebInterfaceEngineer.php /var/www/
+echo "done"
