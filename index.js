49c290966cbe210da1d53972b77ccecf9e159b74
Eric Gelinas <eric@standardpixel.com>
2013-11-02 15:44:25 -0700

    some long overdue fixes

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-var http       = require('http'),
-    express    = require('express'),
-	colors     = require('colors'),
-    app        = express(),
-	config     = require(__dirname + '/config.json'),
-	app_title  = config.app.name,
-	port       = config.app.port;
-
+var http           = require('http'),
+    express        = require('express'),
+	colors         = require('colors'),
+	passport       = require('passport'),
+	BasicStrategy = require('passport-http').BasicStrategy;
+    app            = express(),
+	config         = require(__dirname + '/config.json'),
+	app_title      = config.app.name,
+	port           = config.app.port;
+	
+//
+// Setup Express
+//
 app.set('views', __dirname + '/ui');
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
 
+//
+// Setup Routes
+//
 app.get('/', function(req,res) {
 	res.render('index.html', {
 	 	app_title : app_title
@@ -26,9 +34,18 @@ app.get('/EricGelinas-WebInterfaceEngineer.php', function(req,res) {
 	res.redirect(301, '/redirect');
 });
 
+app.get('/portfolio', function(req,res) {
+	res.render('portfolio.html', {
+	 	app_title : app_title
+	});
+});
+
 app.use('/js', express.static(__dirname + '/ui/js'));
 app.use('/style', express.static(__dirname + '/ui/style'));
 
+//
+// Go Go Go
+//
 app.listen = function(port){
   var server = http.createServer(this);
   console.log('\033[2J');
